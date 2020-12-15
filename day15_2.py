4cf394db33caebb1fdf5c0de73f1e74de5029456
Huiyu <iownthegame@gmail.com>
2020-12-15 20:06:59 +0100

    Solve day15

---
+++ day15_2.py
@@ -0,0 +1,59 @@
+def run(filename):
+    with open(filename) as f:
+        data = f.read().splitlines()
+
+    res = sol(data)
+    print('ans: %s' % res)
+
+
+def sol(data):
+    """
+    0,3,6
+    """
+    res = []
+
+    for line in data:
+        tmp = line.split(',')
+        res.append(game(map(int, tmp)))
+
+    return res
+
+def game(numbers):
+    table = {}
+
+    cnt =  1
+    n = len(numbers)
+    last_number = numbers[-1]
+
+    MAX_TURN = 30000000
+
+    while True:
+        if cnt <= n:
+            table[numbers[cnt-1]] = [cnt]
+            cnt += 1
+            continue
+
+        if not last_number in table or len(table[last_number]) == 1:
+            last_number = 0
+        else:
+            tmp = table[last_number][-1] - table[last_number][-2]
+            last_number = tmp
+
+        if not last_number in table:
+            table[last_number] = [cnt]
+        else:
+            if len(table[last_number]) == 2:
+                table[last_number].pop(0)
+
+            table[last_number].append(cnt)
+
+        cnt += 1
+        if cnt % 10000 == 0:
+            print(cnt)
+
+        if cnt > MAX_TURN:
+            return last_number
+
+if __name__ == '__main__':
+    # run('input/day15_test')
+    run('input/day15')
